3b668957e234b1a6502f71689ad8f2f5f68c9f46
Tai-SE173015 <taibeo161023@gmail.com>
2025-09-23 19:32:11 +0700

    done giao diện wellness hub

--- src/pages/WellnessHub.jsx
+++ src/pages/WellnessHub.jsx
@@ -6,23 +6,24 @@ import RightSection from '../components/molecules/WellnessRight';
 export default function WellnessHub() {
     return (
         <motion.div
-            className="p-4 rounded-lg min-h-screen flex flex-col scrollbar-none"
+            className=" rounded-lg min-h-screen flex flex-col scrollbar-none"
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 0.3 }}
         >
             {/* Header Section */}
-            <div className="flex items-center justify-between mb-4">
+            {/* <div className="flex items-center justify-between mb-4">
                 <h1 className="text-3xl font-bold text-purple-700 dark:text-purple-300">
                     Emo's care corner
                 </h1>
 
-            </div>
+            </div> */}
 
             {/* Body Section (2 parts left, 3 parts right) */}
             <div className="flex flex-1 gap-4 mt-4">
-                <LeftSection />
                 <RightSection />
+
+                <LeftSection />
             </div>
         </motion.div>
     );
